cd213df4dcd60820ff8a7f11549986a97d12c52c
Joshua <el.jdragon@gmail.com>
2026-04-24 08:21:19 -0500

    Rename 'image_url' to 'imageurl' in data append

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ def listar_productos():
                 "id": row[0],
                 "nombre": row[1],
                 "precio": float(row[2]) if row[2] is not None else None,
-                "image_url": row[3],
+                "imageurl": row[3],
             })
 
         return jsonify({
