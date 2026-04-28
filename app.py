4cf5704fd31da54f65a1803caa1cbd5467866d76
Joshua <el.jdragon@gmail.com>
2026-04-28 23:04:18 +0000

    Funcion para que funione el envio de alertas

--- app.py
+++ app.py
@@ -32,7 +32,33 @@ def get_connection():
     )
 
     return connect(connection_string)
+def enviar_correo_alerta(asunto, mensaje destino):
+email_user = os.getenv("EMAIL_USER")
+email_password = os.getenv("EMAIL_PASSWORD")
 
+
+if not email_user:
+raise ValueError("Falta EMAIL_USER")
+if not email_password:
+raise ValueError("Falta EMAIL_PASSWORD")
+
+
+msg = MIMEText(mensaje, "plain"
+msg["Subject"] = asunto
+msg["From"] = email_user
+msg["To"]
+=
+destino
+
+
+"utf-8")
+
+
+servidor = smtplib.SMTP("smtp.gmail.com", 587)
+servidor.starttls()
+servidor.login(email_user, email_password)
+servidor.sendmail(email_user, [destino], msg.as_string())
+servidor.quit()
 
 @app.route("/")
 def home():
